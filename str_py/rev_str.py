149b6197602bdcb3fa41df59321663af3827cc31
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-13 22:11:26 -0500

    reversed a string

---
+++ str_py/rev_str.py
@@ -0,0 +1,23 @@
+'''
+Docstring for rev_str: 
+This is an exercise in reversing a string. So "Hello" would have to
+result as "olleH"
+'''
+
+str_py = "Star Trek"
+rev_str = str_py[::-1]
+print(rev_str)
+
+str_py = "Star Trek"
+rev_str = str_py[::-1].lower()
+print(rev_str)
+# lower() is used to convert all capital letters in a string to 
+# uppercase.
+
+str_py1 = "Star"
+rev_str1 = str_py1[::-1]
+print(rev_str1)
+
+str_py2 = "Trek"
+rev_str2 = str_py2[::-1]
+print(rev_str2)
