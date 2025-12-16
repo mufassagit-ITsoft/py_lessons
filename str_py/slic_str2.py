c7a2999f225015efa377ac493eb4a30c61afcb58
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-16 13:39:00 -0500

    added iteration over string slicing at even indices

--- str_py/slic_str2.py
+++ str_py/slic_str2.py
@@ -12,8 +12,23 @@ printed as "" or print without any changes.
 
 str_py = input("Enter a word: ")
 new_str = ""
+new_str2 = ""
 
 for char in range(len(str_py)):
     if char%2 != 0:
         new_str += str_py[char]
+    elif len(str_py) < 2:
+        new_str += str_py
 print(new_str)
+
+# For indices, to get to the even index, the logic must not equal
+# to zero.
+
+# alternative code
+
+for i in range(1, len(str_py), 2):
+    new_str2 += str_py[i]
+print(new_str2)
+
+# This does the same as the for for loop on range() and len()
+# The difference is in the use of conditional logic.
