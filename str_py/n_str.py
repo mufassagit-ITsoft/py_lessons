bb26bd7fd5030406dc70b9de72b67d5d6d7bb963
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-18 19:44:11 -0500

    added a slightly modified alternative code

--- str_py/n_str.py
+++ str_py/n_str.py
@@ -23,3 +23,19 @@ else:
         if char != ind:
             new_str += str_py[char]
     print(new_str)
+
+# alternative code
+'''
+The slight difference is in "len(str_py) == 0" vs. not str_py. The
+"not str_py" is the truthy or falsy way of saying 
+"if len(str_py) == 0. Perhaps this may be a more pythonic way
+to write this code. 
+'''
+if(not str_py) or (ind >= len(str_py)):
+    print(str_py)
+else:
+    new_str1 = ""
+    for char in range(len(str_py)):
+        if char != ind:
+            new_str1 += str_py[char]
+    print(new_str1)
